0ffb80a6e77dc719dfc30d3056089c2854487e5e
samy <udev9985@gmail.com>
2024-01-31 18:53:51 +0100

    add to page 2 urls projects

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ const URL_R00T = "https://samyferhat.github.io/";
 
 const urls = [
     "tic-tac-toe",
+    "meteo-du-jour-api",
+    "quote-generator-api",
+
 ];
 
-const list= urls.reduce((acc, url) => `<li><a href="${URL_R00T + url}">${url}</a></li>`,"");
+const list= urls.reduce((acc, url) => acc + `<li><a href="${URL_R00T + url}" target="_blank">${url}</a></li>`,"");
 
 let ul = document.createElement("ul");
 
